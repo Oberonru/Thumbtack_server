8e583ecf39e9cc6055d754b20fafc7d10718e216
Alexey <oberonru@mail.ru>
2019-12-12 16:26:48 +0600

    Переделал тест, пользователь создаётся из UserService, класс User служит в качестве модели и всё Не мгу сделать десериализацию

--- src/test/java/TestDataBase.java
+++ src/test/java/TestDataBase.java
@@ -1,3 +1,5 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dao.UserDaoImpl;
 import database.DataBase;
 import model.User;
 import org.junit.Assert;
@@ -5,30 +7,22 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 
 public class TestDataBase {
-    @Test
-    public void test_addUser() throws Exception {
-        DataBase db = new DataBase();
-        Assert.assertEquals(db.getUserList().size(), 0);
-        User user = new User();
-        user.setFirstName("Ijora");
-        user.setLastName("Zajratii");
-        user.setLogin("Zajrati_ijora");
-        user.setPassword("0000");
-        db.addUser(user);
-        Assert.assertEquals(db.getUserList().size(), 1);
-        for (User u : db.getUserList()) {
-            Assert.assertEquals(u.getFirstName(), "Ijora");
-            Assert.assertEquals(u.getLastName(), "Zajratii");
-            Assert.assertEquals(u.getLogin(), "Zajrati_ijora");
-            Assert.assertEquals(u.getPassword(), "0000");
-        }
-        //verify serialize to file
-        Assert.assertTrue(new File("test.txt").exists());
-        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("test.txt")));
-        User deserializableUser = (User) ois.readObject();
-    }
 
+    @Test
+    public void test_addUser() throws IOException {
+        UserService userService = new UserService();
+        User vasek = userService.createUser("Uasja", "Pupyakin", "Pupyan","pup123");
+        UserDaoImpl userDao = new UserDaoImpl();
+        userDao.insert(vasek);
+
+        //Десериализация пользователя
+        ObjectMapper mapper = new ObjectMapper();
+        User deserializeUser = mapper.readValue("test.txt", User.class);
+        Assert.assertEquals(deserializeUser.getFirstName(), "Uasja");
+
+    }
 }
