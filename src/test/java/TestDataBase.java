73f0439812134f80892e425171618c54375b4966
Alexey <oberonru@mail.ru>
2019-12-13 17:22:19 +0600

    Сериализует, десериализует как надо, единственное надо сделать на что он проверяет

--- src/test/java/TestDataBase.java
+++ src/test/java/TestDataBase.java
@@ -1,28 +1,27 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dao.UserDaoImpl;
-import database.DataBase;
 import model.User;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 
 public class TestDataBase {
 
     @Test
     public void test_addUser() throws IOException {
         UserService userService = new UserService();
-        User vasek = userService.createUser("Uasja", "Pupyakin", "Pupyan","pup123");
         UserDaoImpl userDao = new UserDaoImpl();
+        User vasek = userService.createUserWithToken("Uasja", "Pupyakin", "Pupyan","pup123");
+        User boryan = userService.createUserWithToken("Boryaha", "Mordatii","bormord", "8765");
         userDao.insert(vasek);
-
-        //Десериализация пользователя
+        userDao.insert(boryan);
         ObjectMapper mapper = new ObjectMapper();
-        User deserializeUser = mapper.readValue("test.txt", User.class);
-        Assert.assertEquals(deserializeUser.getFirstName(), "Uasja");
-
+        User[] deserializeUser = mapper.readValue(new File("test.txt"), User[].class);
+        for (User u : deserializeUser) {
+            System.out.println(u.getFirstName());
+        }
     }
+
+
 }
