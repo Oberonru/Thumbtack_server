07d7f7b8271dd48cd03e87719259eba7303bce22
Alexey <oberonru@mail.ru>
2019-12-18 15:54:10 +0600

    Реализовал метод registerUser через вызов метода сервиса, добавил проверку через флаг isStarted, на то что сервер уже запущен

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -2,33 +2,29 @@ import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import database.DataBase;
-import model.Song;
-import model.User;
-import request.RegisterSongDtoRequest;
-import request.RegisterUserDtoRequest;
-import response.RegisterSongDtoResponse;
 
 import java.io.File;
 import java.io.IOException;
 
 /**
  * * Будет лучше, если класс Server сам никакие операции выполнять не будет, а будет делегировать их выполнение
- *  * соответствующему классу, который мы назовем классом сервиса (например, UserService, SongService и т.д), вызывая
- *  * соответствующий метод из этого класса. Экземпляры этих классов должны быть полями класса Server.
- *  * <p>
- *  * Итак, класс Server передает полученный json соответствующему классу сервиса. Этот класс должен распарсить json,
- *  * проанализировать и проверить находящийся в нем запрос, выполнить его (конечно, если в запросе нет ошибок) и вернуть
- *  * ответ. Если в запросе имеются ошибки, класс сервиса должен тут же вернуть ответ с полем “error”, не производя
- *  * дальнейшей обработки.
+ * * соответствующему классу, который мы назовем классом сервиса (например, UserService, SongService и т.д), вызывая
+ * * соответствующий метод из этого класса. Экземпляры этих классов должны быть полями класса Server.
+ * * <p>
+ * * Итак, класс Server передает полученный json соответствующему классу сервиса. Этот класс должен распарсить json,
+ * * проанализировать и проверить находящийся в нем запрос, выполнить его (конечно, если в запросе нет ошибок) и вернуть
+ * * ответ. Если в запросе имеются ошибки, класс сервиса должен тут же вернуть ответ с полем “error”, не производя
+ * * дальнейшей обработки.
  */
 public class Server {
 
-    private static UserService userService;
+    private static UserService userService = new UserService();
     private static SongService songService = new SongService();
     private static DataBase db = DataBase.getInstance();
     private static boolean isStarted;
 
     private String token;
+
     /**
      * Производит всю необходимую инициализацию и запускает сервер.
      * savedDataFileName - имя файла, в котором было сохранено состояние сервера.  Если savedDataFileName == null,
@@ -51,9 +47,14 @@ public class Server {
      * @param savedDataFileName
      */
     public void stopServer(String savedDataFileName) throws Exception {
-            isStarted = false;
+        isStarted = false;
     }
 
+    public void registerUser(String registerUserJson) throws Exception {
+        if (isStarted) {
+            userService.registerUser(registerUserJson);
+        }
+    }
 
     public static void main(String[] args) throws Exception {
     }
@@ -70,7 +71,6 @@ public class Server {
         } catch (IOException e) {
         }
     }
-
 
 
 }
